9234e9e7fd99254fe526fbac9ae5c2b11177698e
frankyu02 <frankyu02@yahoo.ca>
2022-02-05 13:10:43 -0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     resolve: 'gatsby-source-sanity',
     options: {
       "projectId": "p67lswln",
-      "dataset": ""
+      "dataset": "production"
     }
   }, "gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
