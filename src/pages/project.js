89c0ac6d9e73fb6327b9dd83b7ef418b4112d599
frankyu02 <frankyu02@yahoo.ca>
2022-02-06 21:29:51 -0500

    moved project Card styling to own file

--- src/pages/project.js
+++ src/pages/project.js
@@ -2,6 +2,9 @@ import { graphql, useStaticQuery } from "gatsby";
 import React from "react";
 
 export default function Projects(){
+    const data = useStaticQuery(graphql`
+    
+    `)
     return(
         <h1>Hey I'm the Projects page!</h1>
     )
