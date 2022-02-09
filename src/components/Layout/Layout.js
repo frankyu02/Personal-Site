fbb141ea49f1352a1b57fc81d423f73277b181e3
frankyu02 <frankyu02@yahoo.ca>
2022-02-09 12:59:37 -0500

    moved  background to be in each component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,17 +4,9 @@ import Footer from "../Footer/Footer";
 import Header from "../header/header";
 import GlobalStyles from "../styles/GlobalStyles";
 
-const LayoutStyled = styled.div`
-    background: rgba(237,253,255,1);
-    position: fixed;
-    z-index: -2;
-    width: 100vw;
-    height: 100vh;
-`;
 export default function Layout(props){
     return(
         <div>
-        <LayoutStyled />
         <GlobalStyles />
         <Header />
         {props.children}
---
+++ src/components/styles/background.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Background = styled.div`
+    background: rgba(237,253,255,1);
+    position: fixed;
+    z-index: -2;
+    width: 100vw;
+    height: 100vh;
+`;
