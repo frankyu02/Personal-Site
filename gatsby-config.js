1f54abeb26849c8164d457fd0c3dac638a41887a
frankyu02 <frankyu02@yahoo.ca>
2022-02-09 12:25:38 -0500

    re-ordered config

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,15 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: [
+    `gatsby-plugin-image`,
+    'gatsby-plugin-sharp', 
+    'gatsby-transformer-sharp', 
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        disableVendorPrefixes: true
+      }
+    },
      {
     resolve: 'gatsby-source-sanity',
     options: {
@@ -17,7 +26,8 @@ module.exports = {
       "dataset": "production",
       token: SANITY_TOKEN,
     }
-  }, "gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  }, 
+  {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
