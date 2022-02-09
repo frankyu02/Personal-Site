a9d646b4c9dcab1e51877a225a3a14c7350c4070
frankyu02 <frankyu02@yahoo.ca>
2022-02-09 13:01:46 -0500

    background moved to indivisual components

--- src/components/styles/background.js
+++ src/components/styles/background.js
@@ -1,9 +1,12 @@
 import styled from "styled-components";
 
-export const Background = styled.div`
+const Background = styled.div`
     background: rgba(237,253,255,1);
     position: fixed;
     z-index: -2;
     width: 100vw;
     height: 100vh;
 `;
+export{
+    Background,
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import HomeHero from "../components/Home/Hero";
 import Welcome from "../components/Home/Welcome";
-import Background from "../components/styles/background";
+import { Background } from "../components/styles/background";
 
 export default function home(){
   return(
     <>
-      <Background />
+      <Background></Background>
       <HomeHero />
       <Welcome />
     </>
