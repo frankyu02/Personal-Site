c66c61e64df97a7c6cd626099948b738115784ca
frankyu02 <frankyu02@yahoo.ca>
2022-05-08 20:14:37 -0400

    SEO improvement

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import SEO from "../components/SEO";
 export default function home(){
   return(
     <div>
-      <SEO title="Homepage" description="The Official Website of Frank Yu. Computer Science student @ UWaterloo!"/>
+      <SEO title="Frank Yu - Personal Web Page" description="The Official Website of Frank Yu. Computer Science student @ UWaterloo!"/>
       <HomeHero />
       <Welcome />
     </div>
